35018eb56b9b210eedfb94c58555788cf9eb185f
BoltzBit <mateus_desktop@outlook.com>
2020-02-08 00:47:28 -0300

    exercicio da noite

---
+++ datastructures/splice.js
@@ -0,0 +1,17 @@
+//metodo para remover uma parte do array de qualquer lugar splice()
+let array = ['today','was','not','so','great']
+array.splice(2,2)
+//o primeiro parametro é onde começa para remover, e o segundo onde termina
+console.log(array)
+
+let array2 = ['I','am','feeleing','really','happy']
+let newArray = array2.splice(3,2)
+//splice retorna os elementos que removeu
+
+console.log(newArray)
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1]
+
+
+
+console.log(arr)
