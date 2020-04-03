2225d85a14650c27a5a05625861e862710f6a9dc
BoltzBit <mateus_desktop@outlook.com>
2020-04-02 22:32:15 -0300

    exercicios freecodecamp

---
+++ functionalProgramming/filterFunction.js
@@ -0,0 +1,14 @@
+const users = [
+    {name: 'John', age:20},
+    {name:'Mateus', age:20},
+    {name:'Gustavo',age:15},
+];
+
+
+const usersUnder30 = users.filter(user => user.age < 30);
+
+console.log(usersUnder30);
+
+let testnum = "4.6";
+
+console.log(testnum === 4.6);
