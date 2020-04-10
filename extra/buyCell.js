9215c2f4a9365f56b8cebafe228122edd423e8ff
BoltzBit <mateus_desktop@outlook.com>
2020-04-10 11:10:39 -0300

    refatorando o codigo buCell

--- extra/buyCell.js
+++ extra/buyCell.js
@@ -1,26 +1,56 @@
 //continuar comprando o celular e verificar se o saldo da conta permite a compra
-let saldoContaBancaria = 8500.95;
-const PRICE_CELL = 800.00;
-const TAXS = 0.12;
-const PRICE_OTHERS = 2.56;
+//refatorar o codigo de acordo com a solucao do livro
 
-let totalCell=0;
-let totalOthers=0;
+const PRICE_CELL = 80.00;
+const TAXS = 0.02;
+const PRICE_OTHERS = 10.00;
+const SPENDING_THRESHOLD = 150.00;
 
-for(let i=0;i<14;i++){
-    totalCell+=PRICE_CELL;
+
+let saldoContaBancaria = 350.95;
+let amount = 0;
+
+//for(let i=0;i<14;i++){
+//    totalCell+=PRICE_CELL;
+//}
+//
+//for(let i=0;i<10;i++){
+//    totalOthers+=PRICE_OTHERS;
+//}
+//
+//let total = totalOthers+totalCell;
+//let totalWithTaxs = total + (total*TAXS);
+//
+//if(totalWithTaxs<saldoContaBancaria){
+//    console.log('Pode Comprar!');
+//    console.log(`Total: $${totalWithTaxs.toFixed(2)}`);
+//}else{
+//    console.log('Saldo insuficiente!');
+//}
+
+//nucleo do programa
+
+
+function calculateTaxs(amount){
+    return amount*TAXS;
 }
 
-for(let i=0;i<10;i++){
-    totalOthers+=PRICE_OTHERS;
+function formatAmount(amount){
+    return `$${amount}`;
 }
 
-let total = totalOthers+totalCell;
-let totalWithTaxs = total + (total*TAXS);
+while(amount < saldoContaBancaria){
+        amount += PRICE_CELL;
 
-if(totalWithTaxs<saldoContaBancaria){
-    console.log('Pode Comprar!');
-    console.log(`Total: $${totalWithTaxs.toFixed(2)}`);
-}else{
-    console.log('Saldo insuficiente!');
+        if(amount<SPENDING_THRESHOLD){
+            amount += PRICE_OTHERS;
+        }
+}
+
+amount +=  calculateTaxs(amount);
+
+console.log(`Sua compra foi de ${formatAmount(amount)}`);
+
+if(amount > saldoContaBancaria){
+    console.log('Você não pode pagar!');
 }
