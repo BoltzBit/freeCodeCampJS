c4270d1508f99edf3949c6d92ea9cbaab7a17e74
BoltzBit <mateus_desktop@outlook.com>
2020-05-06 09:08:35 -0300

    exercicios freecodecamp

--- intermediateAlgorithms/toLowerCase.js
+++ intermediateAlgorithms/toLowerCase.js
@@ -1,13 +1,38 @@
+/*
 function spinalCase(str){
-    
+	//variavel para espaços em branco e underscore
+    let regex = /\s+|_+/g;
+	
+	//separa low-upper para low-space-upper
+	str = str.replace(/([a-z])([A-Z])/g,'$1 $2');
+	
+	return str.replace(regex,'-').toLowerCase();
+}
+*/
 
+//solucao 2
 
-    return str.indexOf(/[A-Z]/);
+/*
+function spinalCase(str){
+	str = str.replace(/([a-z])([A-Z])/g,'$1 $2');
+	
+	return str
+		.toLowerCase()
+		.split(/(?:_| )+/)
+		.join('-');
+}
+*/
+
+//solucao 3
+
+function spinalCase(str){
+	return str
+		.split(/\s|_|(?=[A-Z])/)
+		.join('-')
+		.toLowerCase();
 }
 
-
-
-console.log(spinalCase('thisIsSpinalTap'));
+console.log(spinalCase('The_Andy_Griffith_Show'));
 
 //ideias para resolver
 //regex maiuscula /[A-Z]/
